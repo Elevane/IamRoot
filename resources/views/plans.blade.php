274c112fb9cee4cba2bc63411dca22b468af97ed
Bastien <pma.Bastien.aubry@gmail.com>
2020-02-13 15:22:32 +0100

    confilcts

--- resources/views/plans.blade.php
+++ resources/views/plans.blade.php
@@ -1,26 +1,5 @@
 @extends('base')
 @section('content')
 
-  <section>
-      <h2>$ $ $</h2>
-      <div id="plans-sheet">
-        <div class="plan">
-          <h3>Basic</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.gb</p>
-          <h4>10$/Month</h4>
-        </div>
-
-        <div class="plan">
-          <h3>Premium</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.gb</p>
-          <h4>100$/Month</h4>
-        </div>
-
-        <div class="plan">
-          <h3>UltraMegaGiga Hardcore</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.gb</p>
-          <h4>Your soul</h4>
-        </div>
-      </div>
-  </section>
+    <h1>Working in progress !</h1>
 @endsection
