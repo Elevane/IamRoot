6998d861db8261533bc39ceb5a63b9216dfc1179
Bastien <pma.Bastien.aubry@gmail.com>
2020-02-13 11:45:44 +0100

    HTML : social media images link to websites

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,22 +2,22 @@
 <footer>
     <ul>
         <li>
-            <a href="#">
+            <a href="www.github.com"  target="_blank">
                 <img src="{{ URL::to('/') }}/images/github.png" alt="github_icon">
             </a>
         </li>
         <li>
-            <a>
+            <a href="www.twitter.com" target="_blank">
                 <img src="{{ URL::to('/') }}/images/twitter.png" alt="github_icon">
             </a>
         </li>
         <li>
-            <a>
+            <a href="www.facebook.com" target="_blank">
                 <img src="{{ URL::to('/') }}/images/facebook.png" alt="github_icon">
             </a>
         </li>
         <li>
-            <a>
+            <a href="www.reedit.com" target="_blank">
                 <img src="{{ URL::to('/') }}/images/reddit.png" alt="github_icon">
             </a>
         </li>
