dd9873e333d8a47fbe707ba3eb8c3ce371e273b7
Mathias Guerin <Mathias Guerin>
2020-02-13 10:33:32 +0100

    css: textarea

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
     <section>
-        <h1>IamRoot</h1>
         <form action="/contact/form/{$request}" method="GET">
             <label for="lastname">Lastname</label>
             <input type="text" id="lastname" name="lastname">
@@ -12,7 +11,7 @@
             <label for="email">Email</label>
             <input type="email" id="email" name="email">
             <label for="message">Message</label>
-            <input type="text" id="email" name="email">
+            <textarea name="email" id="email" rows="8" cols="80"></textarea>
             <label for="submit"></label>
             <input type="submit" id="submit" name="submit">
         </form>
