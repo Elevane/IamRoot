fde004379f5c3e1bb88e16e2c62961162e0d0724
Bastien <pma.Bastien.aubry@gmail.com>
2020-02-14 09:34:11 +0100

    HTML : name of the about us part

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,13 +4,13 @@
         <h1>About Us</h1>
         <div id="plans-sheet">
             <div>
-                <h3>Basic</h3>
+                <h3>Chewye</h3>
                 <img class="photos" src="{{ URL::to('/') }}/images/chewye.jpg" alt="chewye_photo"/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.gb</p>
 
             </div>
             <div>
-                <h3>Basic</h3>
+                <h3>Genesis</h3>
                 <img  class="photos" src="{{ URL::to('/') }}/images/ram.jpg" alt="ram_photo"/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.gb</p>
             </div>
